a08bc6e4015cc537c6b170a90c22e829556f29fb
prakharchoudhary <prakhar2397@gmail.com>
2017-06-09 14:04:28 +0530

    add models

---
+++ iQuote/quote/choices.py
@@ -0,0 +1,12 @@
+CATEGORY_CHOICES = (
+		('None', 'None'),
+		('Happy', 'Happy'),
+		('Sad', 'Sad'),
+		('Inspiring', 'Inspiring'),
+		('Romantic', 'Romantic'),
+		('Sarcastic', 'Sarcastic'),
+		('Funny', 'Funny'),
+		('Evil', 'Evil'),
+		('Bravery', 'Bravery'),
+		('Wisdom', 'Wisdom'),
+	)
--- iQuote/quote/models.py
+++ iQuote/quote/models.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
+from .choices import CATEGORY_CHOICES 
 # Create your models here.
+
+class Quote(models.Model):
+
+	created = models.DateTimeField(auto_now_add = True)
+	quote = models.CharField(max_length=140)
+	category = models.CharField(choices=CATEGORY_CHOICES, default='Happy', max_length=100)
+
